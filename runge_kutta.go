31dc8bf0e1f995b875ea4ed321debb10b9556abc
Luis Gargitter <luis.gargitter@gmx.net>
2024-11-14 18:20:16 +0100

    Restructured RK4 to make more readable.

--- runge_kutta.go
+++ runge_kutta.go
@@ -27,24 +27,10 @@ func NewRK4Workspace(n int) *RK4Workspace {
 func RK4(w *RK4Workspace, f System, dt float64, y0 *mgl64.VecN, y *mgl64.VecN) {
 	d, k1, k2, k3, k4 := w.d, w.k1, w.k2, w.k3, w.k4
 	f(y0, k1)
-
-	k1.Mul(d, dt/2.0)
-	y0.Add(d, d)
-	f(d, k2)
-
-	k2.Mul(d, dt/2.0)
-	y0.Add(d, d)
-	f(d, k3)
-
-	k3.Mul(d, dt)
-	y0.Add(d, d)
-	f(d, k4)
+	f(y0.Add(d, k1.Mul(d, dt/2.0)), k2)
+	f(y0.Add(d, k2.Mul(d, dt/2.0)), k3)
+	f(y0.Add(d, k3.Mul(d, dt)), k4)
 
 	// y = y0 + (h/6)(k1 + 2*k2 + 2*k3 + k4)
-	k2.Add(d, k3)
-	d.Mul(d, 2.0)
-	d.Add(d, k1)
-	d.Add(d, k4)
-	d.Mul(d, dt/6.0)
-	y0.Add(y, d)
+	y = y0.Add(y, k2.Add(d, k3).Mul(d, 2.0).Add(d, k1).Add(d, k4).Mul(d, dt/6.0))
 }
