883895d79e4f084ebf1d018f93f219f739283333
Luis Gargitter <luis.gargitter@gmx.net>
2024-11-29 18:43:02 +0100

    export Workspace variables.

--- runge_kutta.go
+++ runge_kutta.go
@@ -5,21 +5,21 @@ type VecMul[T any] func(d *T, a *T, c float64) *T
 
 // Runge Kutta 4
 type RK4Workspace[T any] struct {
-	add VecAdd[T]
-	mul VecMul[T]
-	d   T
-	k1  T
-	k2  T
-	k3  T
-	k4  T
+	Add VecAdd[T]
+	Mul VecMul[T]
+	D   T
+	K1  T
+	K2  T
+	K3  T
+	K4  T
 }
 
 func RK4[T any](w *RK4Workspace[T], f System[T], dt float64, y0 *T, y *T) {
-	f(y0, &w.k1)
-	f(w.add(&w.d, y0, w.mul(&w.d, &w.k1, dt/2.0)), &w.k2)
-	f(w.add(&w.d, y0, w.mul(&w.d, &w.k2, dt/2.0)), &w.k3)
-	f(w.add(&w.d, y0, w.mul(&w.d, &w.k3, dt)), &w.k4)
+	f(y0, &w.K1)
+	f(w.Add(&w.D, y0, w.Mul(&w.D, &w.K1, dt/2.0)), &w.K2)
+	f(w.Add(&w.D, y0, w.Mul(&w.D, &w.K2, dt/2.0)), &w.K3)
+	f(w.Add(&w.D, y0, w.Mul(&w.D, &w.K3, dt)), &w.K4)
 
-	// y = y0 + (h/6)(k1 + 2*k2 + 2*k3 + k4)
-	y = w.add(y, y0, w.mul(&w.d, w.add(&w.d, w.mul(&w.d, w.add(&w.d, &w.k2, &w.k3), 2.0), w.add(&w.d, &w.k1, &w.k4)), dt/6.0))
+	// y = y0 + (h/6)(K1 + 2*K2 + 2*K3 + K4)
+	y = w.Add(y, y0, w.Mul(&w.D, w.Add(&w.D, w.Mul(&w.D, w.Add(&w.D, &w.K2, &w.K3), 2.0), w.Add(&w.D, &w.K1, &w.K4)), dt/6.0))
 }
